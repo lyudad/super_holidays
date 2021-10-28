0b39119422d5103a47348ebd7e63a0c4ab3e181f
Alexey <paet14@gmail.com>
2021-10-29 01:26:30 +0300

    initial project with eslint

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    root: true,
+    extends: [
+        'eslint:recommended',
+        'airbnb-typescript',
+        'airbnb/hooks',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:jest/recommended',
+        'plugin:prettier/recommended'
+    ],
+    plugins: ['react', '@typescript-eslint', 'jest', 'import'],
+    env: {
+        browser: true,
+        es6: true,
+        jest: true
+    },
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json'
+    },
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    rules: {
+        'linebreak-style': 'off',
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }]
+    },
+    ignorePatterns: ["/*.*"],
+};
