11e025f2b39aadb6e326f6ecf8c4780c28fc1829
Alexey <paet14@gmail.com>
2021-10-29 13:17:02 +0300

    rename

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,7 @@
-export default function Empty(): JSX.Element {
-  return (
-    <>
-      <h1>Login</h1>
-    </>
-  );
+export default function Login(): JSX.Element {
+    return (
+        <>
+            <h1>Login</h1>
+        </>
+    );
 }
