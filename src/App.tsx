ec4d4b31e77ab3a653abe062e86077575bfa5b05
Alexey <paet14@gmail.com>
2021-11-02 01:09:14 +0200

    add calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { Switch, Route } from 'react-router-dom';
-import EmptyPage from './components/Empty';
-import Login from './pages/Login';
-
+import DateRangePickerCalendarExample from './components/Calendar/Calendar';
 function App(): JSX.Element {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={EmptyPage} />
-        <Route exact path="/login" component={Login} />
-      </Switch>
+      <DateRangePickerCalendarExample />
     </div>
   );
 }
---
+++ src/components/Calendar/Calendar.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { format } from 'date-fns';
+import { enGB } from 'date-fns/locale';
+import { DateRangePickerCalendar } from 'react-nice-dates';
+import 'react-nice-dates/build/style.css';
+
+export default function DateRangePickerCalendarExample(): JSX.Element {
+  const START_DATE = Date.now();
+  console.log(Date.now().toString());
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+  const [focus, setFocus] = useState(START_DATE);
+  const handleFocusChange = (newFocus: any) => {
+    setFocus(newFocus || START_DATE);
+  };
+  return (
+    <div>
+      <p>
+        Selected start date:{' '}
+        {startDate
+          ? format(startDate, 'dd MMM yyyy', { locale: enGB })
+          : 'none'}
+        .
+      </p>
+      <p>
+        Selected end date:{' '}
+        {endDate ? format(endDate, 'dd MMM yyyy', { locale: enGB }) : 'none'}.
+      </p>
+      <p>Currently selecting: {focus}.</p>
+      <DateRangePickerCalendar
+        startDate={startDate}
+        endDate={endDate}
+        focus={focus}
+        onStartDateChange={setStartDate}
+        onEndDateChange={setEndDate}
+        onFocusChange={handleFocusChange}
+        locale={enGB}
+      />
+    </div>
+  );
+}
