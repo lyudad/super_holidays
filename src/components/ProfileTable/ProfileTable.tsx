5a44848039fbc243c9c89595c8e73b7e84fee8a0
Alexey <paet14@gmail.com>
2021-12-13 12:31:05 +0200

    lint fix

--- src/components/ProfileTable/ProfileTable.tsx
+++ src/components/ProfileTable/ProfileTable.tsx
@@ -1,22 +1,14 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { TypeUserDates } from 'redux/reducers/types';
 import selectors from 'redux/selectors';
 import { FIRST_PAGE, PAGE_SIZE } from './const';
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 
 import { Table } from 'antd';
 
 export default function ProfileTable(): JSX.Element {
   const { dates } = useSelector(selectors.getState);
-
-  const dataExample = dates?.map(e => {
-    return {
-      key: nanoid(),
-      dates: `${e.start_day} - ${e.end_day}`,
-      status: e.status,
-      type: e.type
-    };
-  });
   const [page] = useState<number>(FIRST_PAGE);
   const [pageSize] = useState<number>(PAGE_SIZE);
 
@@ -24,7 +16,14 @@ export default function ProfileTable(): JSX.Element {
     {
       key: '1',
       title: 'Dates',
-      dataIndex: 'dates'
+      dataIndex: 'dates',
+      render: (e: TypeUserDates) => {
+        return (
+          <p>
+            {e.start_day} - {e.end_day}{' '}
+          </p>
+        );
+      }
     },
     {
       key: '3',
@@ -45,7 +44,7 @@ export default function ProfileTable(): JSX.Element {
     <div className="table">
       <Table
         columns={columns}
-        dataSource={dataExample}
+        dataSource={dates}
         pagination={{
           current: page,
           pageSize: pageSize
